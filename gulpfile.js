9469f4e10497aaeabbe3917f58bd77689397b65f
Santiago Petrone <santiago.petrone@suse.com>
2016-06-29 18:48:20 +0200

    Fix lines length

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,25 @@ var gulp = require('gulp');
 // The require-dir module will automatically include all the files in under the
 // gulp/ folder and require() all the modules in this file
 var requireDir = require('require-dir'),
-    dir = requireDir('./gulp');
+  dir = requireDir('./gulp');
 
 // Build
-gulp.task('build', ['jsBowerExtract', 'cssBowerExtract', 'fontsBowerExtract', 'app', 'less', 'images', 'angularCore', 'angularWidgets', 'angularFeatures', 'angularData', 'watch', 'cleanAssetsHtml']);
+gulp.task('build', [
+  'jsBowerExtract',
+  'cssBowerExtract', 
+  'fontsBowerExtract',
+  'app',
+  'less',
+  'images',
+  'angularCore',
+  'angularWidgets',
+  'angularFeatures',
+  'angularData',
+  'watch',
+  'cleanAssetsHtml'
+]);
 
 // Default task
 gulp.task('default', ['clean'], function () {
-  gulp.start('build');
+	gulp.start('build');
 });
