58459173d74e1200ae73ef16c2cf1c394095d2fb
Santiago Petrone <santiago.petrone@suse.com>
2016-07-01 17:56:06 +0200

    Implement translations for Upgrade flow

--- assets/app/crowbar-app.module.js
+++ assets/app/crowbar-app.module.js
@@ -24,47 +24,16 @@
     // lodash support
     .constant('_', _);
 
-  configuration.$inject = ['$urlRouterProvider', '$translateProvider'];
+  configuration.$inject = ['$urlRouterProvider'];
 
-  function configuration($urlRouterProvider, $translateProvider) {
+  function configuration($urlRouterProvider) {
 
     configureRouter();
-    configureTranslate();
 
     //@todo: SOC - Store routing configurations into separate files
     function configureRouter() {
       // For any unmatched url, redirect to /upgrade/prepare
       $urlRouterProvider.otherwise('/upgrade/prepare');
-    }
-
-    //@todo: SOC - Store translation keys into separate files
-    function configureTranslate() {
-      $translateProvider.translations('en', {
-        'dashboard': {
-          'title': 'UI/UX Dashboard Page'
-        },
-        'about': {
-          'title': 'About Page'
-        },
-        'header': {
-          'title': 'SUSE UI/UX Dashboard',
-          'search': {
-            'placeholder': 'Search...',
-            'button': 'Go!'
-          }
-        },
-        'menu': {
-          'home': 'Home',
-          'about': 'About',
-          'dashboard': 'Dashboard'
-        },
-        'footer': {
-          'copyright': 'SUSE - 2016'
-        }
-      });
-
-      $translateProvider.preferredLanguage('en');
-      $translateProvider.useSanitizeValueStrategy('escaped');
     }
   }
 })(_);
--- assets/app/features/upgrade/upgrade.config.js
+++ assets/app/features/upgrade/upgrade.config.js
@@ -6,64 +6,71 @@
     .config(configuration);
 
    
-  configuration.$inject = ['$stateProvider', '$urlRouterProvider'];
+  configuration.$inject = ['$stateProvider', '$urlRouterProvider', '$translateProvider'];
+  // @ngInject
+  function configuration($stateProvider, $urlRouterProvider, $translateProvider) {
 
-  function configuration($stateProvider, $urlRouterProvider) {
+console.log($translateProvider);
+    /**
+     * Configure States, Views and Urls
+     */
+    $stateProvider
+      .state('upgrade', {
+        url: '/upgrade',
+        templateUrl: 'app/features/upgrade/templates/upgrade.html',
+        controller: 'UpgradeCtrl',
+        controllerAs: 'upgradeVm'
+      })
+      .state('upgrade.prepare', {
+        url: '/prepare',
+        templateUrl: 'app/features/upgrade/templates/prepare.html'
+      })
+      .state('upgrade.backup', {
+        url: '/backup',
+        templateUrl: 'app/features/upgrade/templates/backup.html'
+      })
+      .state('upgrade.reinstall-admin', {
+        url: '/reinstall-admin',
+        templateUrl: 'app/features/upgrade/templates/reinstall-admin.html'
+      })
+      .state('upgrade.continue-upgrade', {
+        url: '/continue-upgrade',
+        templateUrl: 'app/features/upgrade/templates/continue-upgrade.html'
+      })
+      .state('upgrade.restore-admin', {
+        url: '/restore-admin',
+        templateUrl: 'app/features/upgrade/templates/restore-admin.html'
+      })
+      .state('upgrade.verify-repos', {
+        url: '/verify-repos',
+        templateUrl: 'app/features/upgrade/templates/verify-repos.html'
+      })
+      .state('upgrade.stop-openstack-services', {
+        url: '/stop-openstack-services',
+        templateUrl: 'app/features/upgrade/templates/stop-openstack-services.html'
+      })
+      .state('upgrade.openstack-backup', {
+        url: '/openstack-backup',
+        templateUrl: 'app/features/upgrade/templates/openstack-backup.html'
+      })
+      .state('upgrade.upgrade-nodes-os', {
+        url: '/upgrade-nodes-os',
+        templateUrl: 'app/features/upgrade/templates/upgrade-nodes-os.html'
+      })
+      .state('upgrade.finishing-upgrade', {
+        url: '/finishing-upgrade',
+        templateUrl: 'app/features/upgrade/templates/finishing-upgrade.html'
+      });
 
-    configureRouter();
-
-    function configureRouter() {
-
-      // Now set up the states
-      $stateProvider
-        .state('upgrade', {
-          url: '/upgrade',
-          templateUrl: 'app/features/upgrade/templates/upgrade.html',
-          controller: 'UpgradeCtrl',
-          controllerAs: 'upgradeVm'
-        })
-        .state('upgrade.prepare', {
-          url: '/prepare',
-          templateUrl: 'app/features/upgrade/templates/prepare.html'
-        })
-        .state('upgrade.backup', {
-          url: '/backup',
-          templateUrl: 'app/features/upgrade/templates/backup.html'
-        })
-        .state('upgrade.reinstall-admin', {
-          url: '/reinstall-admin',
-          templateUrl: 'app/features/upgrade/templates/reinstall-admin.html'
-        })
-        .state('upgrade.continue-upgrade', {
-          url: '/continue-upgrade',
-          templateUrl: 'app/features/upgrade/templates/continue-upgrade.html'
-        })
-        .state('upgrade.restore-admin', {
-          url: '/restore-admin',
-          templateUrl: 'app/features/upgrade/templates/restore-admin.html'
-        })
-        .state('upgrade.verify-repos', {
-          url: '/verify-repos',
-          templateUrl: 'app/features/upgrade/templates/verify-repos.html'
-        })
-        .state('upgrade.stop-openstack-services', {
-          url: '/stop-openstack-services',
-          templateUrl: 'app/features/upgrade/templates/stop-openstack-services.html'
-        })
-        .state('upgrade.openstack-backup', {
-          url: '/openstack-backup',
-          templateUrl: 'app/features/upgrade/templates/openstack-backup.html'
-        })
-        .state('upgrade.upgrade-nodes-os', {
-          url: '/upgrade-nodes-os',
-          templateUrl: 'app/features/upgrade/templates/upgrade-nodes-os.html'
-        })
-        .state('upgrade.finishing-upgrade', {
-          url: '/finishing-upgrade',
-          templateUrl: 'app/features/upgrade/templates/finishing-upgrade.html'
-        });
-
-    }
+    /**
+     * Configure Translations
+     */
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'app/features/upgrade/i18n/',
+        suffix: '.json'
+      });
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.fallbackLanguage('en');
+    $translateProvider.useSanitizeValueStrategy('sanitize');
   }
 })(_);
-
