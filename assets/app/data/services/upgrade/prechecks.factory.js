e7d274d3fd24a9954ac9892be986c4f9cf473f80
Santiago Petrone <santiago.petrone@suse.com>
2016-07-13 17:32:57 +0200

    Landing page for Upgrade7 + prechecks service integration

---
+++ assets/app/data/services/upgrade/prechecks.factory.js
@@ -0,0 +1,32 @@
+(function() {
+
+  angular
+    .module('crowbarData')
+    .factory('prechecksFactory', prechecksFactory);
+
+  /* @ngInject */
+  function prechecksFactory($q, $http) {
+    var factory = {
+      getAll: getStepsFactory,
+      getAllStatic: function() {
+        return {
+          "errors": ["001", "002", "003"]
+        };
+      }
+    };
+
+    return factory;
+    function getStepsFactory(forceFailure = false) {
+
+      var requestOptions = {
+        method: 'GET',
+        url: '/api/upgrade7/prechecks'
+      };
+
+      if (forceFailure) {
+        requestOptions.url += '?fail=' + forceFailure;
+      }
+      return $http(requestOptions);
+    }
+  }
+})();
