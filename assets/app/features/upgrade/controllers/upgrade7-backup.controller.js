906248d254b7673a144ebd7364f8c8600d66e069
Santiago Petrone <santiago.petrone@suse.com>
2016-07-22 15:45:28 +0200

    Update Backup step

---
+++ assets/app/features/upgrade/controllers/upgrade7-backup.controller.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name crowbarApp.controller:Upgrade7BackupCtrl
+   * @description
+   * # Upgrade7BackupCtrl
+   * This is the controller used on the Upgrade landing page
+   */
+  angular.module('crowbarApp')
+    .controller('Upgrade7BackupCtrl', Upgrade7BackupCtrl);
+
+  Upgrade7LandingCtrl.$inject = ['$scope', '$translate', '$state'];
+  // @ngInject
+  function Upgrade7BackupCtrl($scope, $translate, $state) {
+    var controller = this;
+    controller.beginUpdate = beginUpdate;
+
+    /**
+     * Move to the next available Step
+     */
+    function beginUpdate() {
+      // Only move forward if all prechecks has been executed and passed.
+      if (!controller.prechecks.completed || controller.prechecks.errors) {
+        return;
+      }
+
+      $state.go('upgrade7.backup');
+    };
+  }
+})();
---
+++ assets/app/features/upgrade/controllers/upgrade7-landing.controller.js
@@ -0,0 +1,69 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name crowbarApp.controller:Upgrade7LandingCtrl
+   * @description
+   * # Upgrade7LandingCtrl
+   * This is the controller used on the Upgrade landing page
+   */
+  angular.module('crowbarApp')
+    .controller('Upgrade7LandingCtrl', Upgrade7LandingCtrl);
+
+  Upgrade7LandingCtrl.$inject = ['$scope', '$translate', '$state', 'prechecksFactory'];
+  // @ngInject
+  function Upgrade7LandingCtrl($scope, $translate, $state, prechecksFactory) {
+    var controller = this;
+    controller.beginUpdate = beginUpdate;
+
+    controller.prechecks = {
+      completed: false,
+      runPrechecks: runPrechecks
+    };
+
+    //@TODO: Remove this once the precheck integration is completed.
+    controller.forcePrecheckFailure = true;
+
+    //Run prechecks
+    controller.prechecks.runPrechecks(controller.forcePrecheckFailure);
+
+    //@TODO: Remove this once the precheck integration is completed.
+    controller.forcePrecheckFailure = false;
+
+    /**
+     * Move to the next available Step
+     */
+    function beginUpdate() {
+      // Only move forward if all prechecks has been executed and passed.
+      if (!controller.prechecks.completed || controller.prechecks.errors) {
+        return;
+      }
+
+      $state.go('upgrade7.backup');
+    };
+
+    /**
+     * Pre validation checks
+     */
+    function runPrechecks(forceFailure = false) {
+      prechecksFactory
+        .getAll(forceFailure)
+        .then(
+          //Success handler. Al precheck passed successfully:
+          function(prechecksResponse) {
+            delete controller.prechecks.errors;
+          },
+          //Failure handler:
+          function(errorPrechecksResponse) {
+            controller.prechecks.errors = errorPrechecksResponse.data.errors;
+          }
+        ).finally(
+          function() {
+            controller.prechecks.completed = true;
+          }
+        );
+
+    };
+  }
+})();
