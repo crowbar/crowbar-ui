8fb50fadaaa3a658754c0b3089ce9ca95448d159
Santiago Petrone <santiago.petrone@suse.com>
2016-08-10 12:16:51 +0200

    Update Gulp file to run some tasks in serie, Update travis.yml file

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,38 @@
 // Include gulp
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    runSequence = require('run-sequence');
 
 // The require-dir module will automatically include all the files in under the
 // gulp/ folder and require() all the modules in this file
-var requireDir = require('require-dir'),
-  dir = requireDir('./gulp');
+var requireDir = require('require-dir');
+requireDir('./gulp');
 
 // Build
-gulp.task('build', [
-  'jsBowerExtract',
-  'cssBowerExtract', 
-  'fontsBowerExtract',
-  'app',
-  'less',
-  'images',
-  'angularCore',
-  'angularWidgets',
-  'angularFeatures',
-  'angularData',
-  'watch',
-  'cleanAssetsHtml'
-]);
+gulp.task('build', function (callback) {
+    runSequence(
+        [
+            'jsBowerExtract',
+            'cssBowerExtract',
+            'fontsBowerExtract',
+            'app',
+            'less',
+            'images',
+            'angularCore',
+            'angularWidgets',
+            'angularFeatures',
+            'angularData'
+        ],
+        'cleanAssetsHtml',
+        callback
+    );
+});
 
 // Default task
-gulp.task('default', ['clean'], function () {
-  gulp.start('build');
+gulp.task('default', function (callback) {
+    runSequence(
+        'clean',
+        'build',
+        'watch',
+        callback
+    );
 });
