77c29c07d5898ede27c684fddb79236bdb635bf6
Santiago Petrone <santiago.petrone@suse.com>
2016-08-19 12:30:32 +0200

    Add unit test base

---
+++ assets/app/features/upgrade/controllers/upgrade7-create-connect-database.controller.spec.js
@@ -0,0 +1,41 @@
+describe('Upgrade Flow - Create Connect Database Controller', function () {
+
+    it('should exist', function () {});
+
+    describe('nextStep function', function () {
+        it('should be defined', function () {});
+
+        it('should redirect the user to "Add Ons Repo Checks" page when admin upgrade is successfull', function () {});
+
+        it('should retain the user on the curent page until the database creation is completed', function () {});
+    });
+
+    it('should have an createConnectDatabase model defined', function () {});
+
+    it('should display "Username" field', function () {});
+
+    it('should display "Password" field', function () {});
+
+    describe('Connect to Database tab is enabled', function () {
+
+        it('should display "Server" field', function () {});
+
+        it('should display "Port" field', function () {});
+
+        it('should display "Table Prefix" field', function () {});
+
+        it('should display "Test Connection" button', function () {});
+    });
+
+    describe('Create Database tab is enabled', function () {
+
+        it('should hide "Server" field', function () {});
+
+        it('should hide "Port" field', function () {});
+
+        it('should hide "Table Prefix" field', function () {});
+
+        it('should display "Create Database" button', function () {});
+
+    });
+});
