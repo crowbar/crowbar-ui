86d1d9d495a0b2b63e400e92174509a02f53ae14
Santiago Petrone <santiago.petrone@suse.com>
2016-08-26 15:28:35 +0200

    Add base for Upgrade7 test

--- assets/app/features/upgrade/upgrade7.controller.spec.js
+++ assets/app/features/upgrade/upgrade7.controller.spec.js
@@ -1,11 +1,26 @@
 describe('Upgrade 7 Controller', function () {
 
+    it('should be defined', function () {});
+
+    describe('Navigation bar', function () {
+        it('should have a Next button defined', function () {});
+        it('should have a Back button defined', function () {});
+        it('should have a Cancel button defined', function () {});
+
+        it('should calculate last Step', function () {});
+        it('should allow to move to the next step', function () {});
+    });
+    describe('Steps sidebar', function () {
+        it('should be defined', function () {});
+    });
 
     //@TODO: Implement the following tests
     describe('cancelUpgrade function', function () {
         it('should be defined', function () {});
 
-        it('should be enabled', function () {});
+        it('should be enabled before step 4', function () {});
+
+        it('should be disabled after step 4', function () {});
 
         describe('cancel modal', function () {
             it('should be displayed when cancel button is clicked', function () {});
