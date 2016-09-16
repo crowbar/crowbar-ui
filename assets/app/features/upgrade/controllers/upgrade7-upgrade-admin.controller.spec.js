652b888c88bd2be04277699b9529d16ca39a2d84
Santiago Petrone <santiago.petrone@suse.com>
2016-09-16 11:21:34 +0200

    Oops, reenable all unit tests

--- assets/app/features/upgrade/controllers/upgrade7-upgrade-admin.controller.spec.js
+++ assets/app/features/upgrade/controllers/upgrade7-upgrade-admin.controller.spec.js
@@ -96,7 +96,7 @@ describe('Upgrade Flow - Upgrade Admin Server Controller', function () {
                     $rootScope.$digest();
                 });
 
-                fit('should leave running at false', function () {
+                it('should leave running at false', function () {
                     assert.isFalse(controller.adminUpgrade.running);
                 });
 
