c4649011d75aad2a0b4833e314c26e06318c8a81
Santiago Petrone <santiago.petrone@suse.com>
2016-10-04 17:30:22 +0200

    Update rsync script and options

--- gulp/rsync.js
+++ gulp/rsync.js
@@ -3,20 +3,45 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     rsync = require('gulp-rsync'),
     optional = require('optional'),
-    syncCloudConfig = optional('./cloud.conf.json') || {},
+    syncCloudConfig = optional('./cloud.conf.json'),
     syncCloudID = gutil.env['sync-cloud'] || 'default',
-    syncClouds = syncCloudConfig.clouds || {},
-    syncCloud = syncClouds[syncCloudID];
+    noSync = gutil.env['no-sync'] || false,
+    syncClouds = syncCloudConfig ? syncCloudConfig.clouds : {},
+    syncCloud = syncClouds ? syncClouds[syncCloudID] : undefined,
+
+    skypeRsync = 'Skypping rsync.';
 
 
 gulp.task('rsync', function () {
-    if (syncCloud) {
+    // Skip rsync option
+    if (noSync) {
+        gutil.log(gutil.colors.yellow(skypeRsync));
+
+    // Missing cloud.config.json file
+    } else if (!syncCloudConfig) {
+        gutil.log(gutil.colors.yellow('Configuration file cloud.config.json not found.', skypeRsync));
+
+    // cloud.config.json file exists, but doesn't contain a 'clouds' attribute
+    } else if (typeof syncCloudConfig.clouds === 'undefined') {
+        gutil.log(gutil.colors.red('Missing clouds on configuration file', skypeRsync));
+
+    // The specified cloud doesn't exist in cloud.config.json file
+    } else if (typeof syncCloud === 'undefined') {
+        gutil.log(gutil.colors.red('Undefined configuration for cloud: ' + syncCloudID, skypeRsync));
+
+    // A non-string host has been specified
+    } else if (typeof syncCloud.host !== 'string') {
+        gutil.log(gutil.colors.red('No valid host specified for curent cloud: ' + syncCloudID, skypeRsync));
+
+    } else {
         // fallback to default port if not defined
         syncCloud.port = syncCloud.port || 22;
 
-        gutil.log('syncing with ' + syncCloudID +
-            ' cloud: ' + syncCloud.host +
-            ':' + syncCloud.port  + '...');
+        gutil.log(
+            gutil.colors.green('syncing with ' + syncCloudID +
+                ' cloud: ' + syncCloud.host +
+                ':' + syncCloud.port  + '...')
+        );
 
         return gulp.src('public/**')
             .pipe(rsync({
@@ -28,5 +53,6 @@ gulp.task('rsync', function () {
                 recursive: true,
                 silent: !syncCloudConfig.options.verbose
             }));
+
     }
 });
