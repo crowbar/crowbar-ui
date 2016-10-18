90bb9e7f85dcc88143db58eb94944e6a1a1dd506
richa <richa.bisht@suse.com>
2016-10-18 10:25:33 +0200

    Features/upgrade7/upgrade nodes/template
    * Create upgrade-nodes page template
    
    * Update upgrade-nodes state in steps.factory and removed test copy from upgrade-nodes-page
    
    * correction in comment text
    
    * Remove empty trailing spaces

---
+++ assets/app/features/upgrade/controllers/upgrade-nodes.controller.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    /**
+    * @ngdoc function
+    * @name crowbarApp.controller:UpgradeNodesController
+    * @description
+    * # UpgradeNodesController
+    * This is the controller used on the Upgrade Nodes step
+    */
+    angular.module('crowbarApp')
+        .controller('UpgradeNodesController', UpgradeNodesController);
+
+    UpgradeNodesController.$inject = [];
+    // @ngInject
+    function UpgradeNodesController() {
+    }
+})();
