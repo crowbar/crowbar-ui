c6dcff23383f1e3f45386a885e4de8b124b8c68e
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2016-10-18 11:54:47 +0200

    Updated service integration for the landing page.

--- routes/api/upgrade/prechecks.js
+++ routes/api/upgrade/prechecks.js
@@ -9,10 +9,11 @@ router.get('/', function(req, res) {
         res.status(500).json({'errors': errors});
     } else {
         res.status(200).json({
-            'updates_installed': true,
-            'network_sanity': true,
-            'high_availability': true,
-            'free_node_available': true
+            'maintenance_updates_installed': { required: true, passed: true },
+            'network_checks': { required: true, passed: true },
+            'clusters_healthy': { required: true, passed: true },
+            'ceph_healthy': { required: true, passed: true },
+            'compute_resources_available': { required: false, passed: true }
         });
     }
 });
