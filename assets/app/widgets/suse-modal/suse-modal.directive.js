947ae504099c60a00472ae46662cec120033a7c9
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2017-01-05 14:42:41 +0100

    upgrade: Common error format in modal
    The error modal was modified to accept error information in format
    commonly returned from the API.

--- assets/app/widgets/suse-modal/suse-modal.directive.js
+++ assets/app/widgets/suse-modal/suse-modal.directive.js
@@ -19,15 +19,22 @@
         return {
             restrict: 'E',
             scope: {
-                error: '='
+                error: '=',
+                translationPrefix: '@',
             },
             link: function (scope) {
+                // init optional binding values
+                scope.translationPrefix = scope.translationPrefix || 'modal';
+
                 scope.$watch('error', function(value) {
                     if (angular.isDefined(value)) {
                         $uibModal.open({
                             templateUrl: 'app/widgets/suse-modal/suse-modal.directive.html',
                             controller: modalController,
                             scope: scope
+                        }).closed.then(function () {
+                            // clear the errors when window is closed
+                            scope.error = undefined;
                         });
 
                     }
