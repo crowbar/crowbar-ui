79e894004e1006ba66cf3140df7b7b2155391283
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2017-02-07 12:58:47 +0100

    upgrade: Pass status response to postSync callback
    The logic in postSync callback might require access to the response
    received from the status call. Expose this as an argument to the
    `postSync()` callback.

--- assets/app/data/crowbar/services/upgrade-status.factory.js
+++ assets/app/data/crowbar/services/upgrade-status.factory.js
@@ -43,7 +43,7 @@
                         }
 
                         if (angular.isFunction(postSync)) {
-                            postSync();
+                            postSync(response);
                         }
 
                     }
